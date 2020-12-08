a9c9541b97ef75483b6ceaa1b3adcd5c8fea2ed3
b.saintot <bsaintot@octo.com>
2020-12-08 10:36:54 +0100

    Refacto day 8

--- day_08/solution.py
+++ day_08/solution.py
@@ -6,17 +6,16 @@ def parse_instructions(file_path):
     return [line.replace('\n', '').split() for line in f.readlines()]
 
 
-def func1(instructions):
+def get_accumulator_value_before_second_execution(instructions):
     accumulator, _ = execute_instructions(instructions)
     return accumulator
 
 
-def func2(instructions):
+def get_accumulator_value_after_termination(instructions):
     indexes_to_invert = [i for i, value in enumerate(instructions) if (value[0] == 'nop') or (value[0] == 'jmp')]
-
     for index_to_invert in indexes_to_invert:
-        accumulator, problem_solved = execute_instructions(instructions, index_to_invert)
-        if problem_solved:
+        accumulator, program_terminated = execute_instructions(instructions, index_to_invert)
+        if program_terminated:
             return accumulator
 
 
@@ -24,10 +23,10 @@ def execute_instructions(instructions, index_to_invert=None):
     i = 0
     is_executed = []
     accumulator = 0
-    problem_solved = False
+    program_terminated = False
     while i not in is_executed:
         if i == len(instructions):
-            problem_solved = True
+            program_terminated = True
             break
 
         if instructions[i][0] == 'nop' and i == index_to_invert:
@@ -47,11 +46,11 @@ def execute_instructions(instructions, index_to_invert=None):
             is_executed.append(i)
             i += int(instructions[i][1])
 
-    return accumulator, problem_solved
+    return accumulator, program_terminated
 
 
 if __name__ == '__main__':
     input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
     instructions_data = parse_instructions(input_file_path)
-    print('Solution 1: ', func1(instructions_data))  # 1818
-    print('Solution 2: ', func2(instructions_data))  # 631
+    print('Solution 1: ', get_accumulator_value_before_second_execution(instructions_data))  # 1818
+    print('Solution 2: ', get_accumulator_value_after_termination(instructions_data))  # 631
