e2ed08ea4ef6b3857b6a4182441e1007ef0f86e7
b.saintot <bsaintot@octo.com>
2020-12-09 09:46:52 +0100

    Refacto day 09

--- day_09/solution.py
+++ day_09/solution.py
@@ -8,19 +8,21 @@ def parse_xmas_numbers(file_path: str) -> List[int]:
 
 
 def find_first_number_not_sum_of_two_of_previous_25_numbers(xmas_numbers: List[int]) -> int:
-    for i in range(0, len(xmas_numbers[25:])):
-        previous_numbers = xmas_numbers[i:25 + i]
-        current_number = xmas_numbers[25:][i]
-        sum_found_in_previous_numbers = False
+    previous_list_size = 25
+    for i in range(0, len(xmas_numbers[previous_list_size:])):
+        previous_numbers = xmas_numbers[i:previous_list_size + i]
+        current_number = xmas_numbers[previous_list_size:][i]
 
-        for x in range(0, len(previous_numbers)):
-            for y in range(x + 1, len(previous_numbers)):
-                if previous_numbers[x] + previous_numbers[y] == current_number:
-                    sum_found_in_previous_numbers = True
-                    break
-
-        if not sum_found_in_previous_numbers:
+        if not sum_in_previous_numbers(current_number, previous_numbers):
             return current_number
+
+
+def sum_in_previous_numbers(current_number: int, previous_numbers: List[int]) -> bool:
+    for x in range(0, len(previous_numbers)):
+        for y in range(x + 1, len(previous_numbers)):
+            if previous_numbers[x] + previous_numbers[y] == current_number:
+                return True
+    return False
 
 
 def find_encryption_weakness(xmas_numbers: List[int]) -> int:
@@ -30,11 +32,11 @@ def find_encryption_weakness(xmas_numbers: List[int]) -> int:
         current_sum = 0
         for j in range(i, len(xmas_numbers)):
             current_sum += xmas_numbers[j]
+            if current_sum > sum_to_find:
+                break
             if current_sum == sum_to_find:
                 result_list = [xmas_numbers[x] for x in range(i, j + 1)]
                 return min(result_list) + max(result_list)
-            if current_sum > sum_to_find:
-                break
 
 
 if __name__ == '__main__':
---
+++ solution_template.py
@@ -0,0 +1,20 @@
+import os
+
+
+def parse_input(input_file_path):
+    pass
+
+
+def solution1(input_data):
+    pass
+
+
+def solution2(input_data):
+    pass
+
+
+if __name__ == '__main__':
+    input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+    input_data = parse_input(input_file_path)
+    print('Solution 1: ', solution1(input_data))  #
+    # print('Solution 2: ', solution2(input_data))  #
